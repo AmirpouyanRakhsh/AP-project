5c133117970606a08fa1a1c658b4d82ee63714b6
Amirpouyan Rakhsh <58786306+AmirpouyanRakhsh@users.noreply.github.com>
2020-01-29 22:31:43 +0330

    3

---
+++ app/src/main/java/com/example/approject/SIGN_UP_LOG_IN.kt
@@ -0,0 +1,13 @@
+package com.example.approject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SIGN_UP_LOG_IN : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign__up__log__in);
+
+    }
+}
